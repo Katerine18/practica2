7b8ee4eb8abf92511bc5de4923fe2eb0737f5375
Katerine18 <ycubero10366@ufide.ac.cr>
2023-09-30 02:02:40 -0600

    practica 2

---
+++ src/main/java/com/practica2/TiendaApplication.java
@@ -0,0 +1,14 @@
+package com.practica2;
+
+import org.springframework.boot.SpringApplication;
+import static org.springframework.boot.SpringApplication.run;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaApplication {
+
+	public static void main(String[] args) {
+		run(TiendaApplication.class, args);
+	}
+
+}
